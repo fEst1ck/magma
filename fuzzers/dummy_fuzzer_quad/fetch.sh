fee6a41e991ea11c1a7f1ae16cbf89db7f1111e4
Zekun Wang <zekun.wang@uwaterloo.ca>
2025-08-26 22:48:56 -0400

    update

--- fuzzers/dummy_fuzzer_quad/fetch.sh
+++ fuzzers/dummy_fuzzer_quad/fetch.sh
@@ -13,6 +13,6 @@ git clone --no-checkout https://github.com/fEst1ck/path-cov-instr.git "$FUZZER/p
 git -C "$FUZZER/path-cov-instr" checkout f35332c0b569c39168a2b2c7f0c098ad54f3fc89
 
 git clone --no-checkout https://github.com/fEst1ck/coverage-playground.git "$FUZZER/repo"
-git -C "$FUZZER/repo" checkout 6ec44bafb43c769a128e405e1f28972571032624
+git -C "$FUZZER/repo" checkout a857280e36a9a1a4f05e40623fd8c3f2f9e08094
 
 git clone https://github.com/fEst1ck/fuzz-target.git "$FUZZER/fuzz-target"
